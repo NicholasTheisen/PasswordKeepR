6a0bb32d8a68a652ef689a3aefd2f9f0e89946b4
NicholasTheisen <nicholas.theisen1997@gmail.com>
2024-03-08 07:43:16 +0000

    generatePassword helper function

---
+++ helpers/generatePassword.js
@@ -0,0 +1,26 @@
+function generatePassword(website) {
+  const capitalLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+  const numbers = '0123456789';
+  const specialCharacters = '!@#$%^&*()_+{}[]';
+
+  let password = '';
+
+  // Add at least one capital letter for the specific website
+  const randomCapitalLetter = capitalLetters.charAt(Math.floor(Math.random() * capitalLetters.length));
+  password += randomCapitalLetter;
+
+  // Generate the rest of the password
+  const allChars = capitalLetters + lowerCaseLetters + numbers + specialCharacters;
+  for (let i = 0; i < 11; i++) {
+    const randomChar = allChars.charAt(Math.floor(Math.random() * allChars.length));
+    password += randomChar;
+  }
+
+  // Shuffle the characters in the password
+  password = password.split('').sort(() => Math.random() - 0.5).join('');
+
+  return password;
+}
+
+module.exports = generatePassword;
